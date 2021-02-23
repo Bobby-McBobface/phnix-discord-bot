31480ee3466454c4418e3d883679c620744f44fb
Bobby-McBobface <54757965+Bobby-McBobface@users.noreply.github.com>
2021-02-23 21:15:08 +0800

    Update levels.py

--- levels.py
+++ levels.py
@@ -1,11 +1,11 @@
 import asyncio
-import
+import level_dict
 interval = 20 # Seconds
 chatted = []
 
 async def level_up(member):
   if member not in chatted:
-    
+    #logic here
     chatted.append(member)
   
   
