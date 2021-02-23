0f45dbf5114e014facea7a7b7e143c0ccc0ff345
LeeSpork <lblogan@outlook.co.nz>
2021-02-23 21:14:31 +1300

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,26 @@
 import discord
 
-#do bot things here
+PREFIX = "$"
+
+def check_for_and_strip_prefixes(string:str, prefixes:tuple) -> str:
+    for prefix in prefixes:
+        if string.startswith(prefix):
+            return string[len(prefix):].lstrip()
+    # If the loop ended, it failed to find a prefix
+    return None
+    
+
+class PhnixBotClient(discord.Client):
+    async def on_message(self, message):
+        """Runs every time the bot notices a message being sent anywhere."""
+        
+        # Ignore bot accounts
+        if message.author.bot == True: return
+        
+        # Check if it has our command prefix, or starts with a mention of our bot
+        command_text = check_for_and_strip_prefixes(message.content, (PREFIX, self.user.mention, f"<@!{self.user.id}>"))
+        if command_text is not None:
+            
+            # TODO: add command logic here
+            await message.channel.send(command_text)
+        
