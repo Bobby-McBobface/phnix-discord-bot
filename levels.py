64c2227a7a7f62f367fd745d9503688a2a255831
Bobby-McBobface <>
2021-02-24 18:11:49 +0800

    Made leveling system work

--- levels.py
+++ levels.py
@@ -1,11 +1,26 @@
 import asyncio
-import level_dict
-interval = 20 # Seconds
+import data
+INTERVAL = 30 # Seconds apart messages have to be
+EXP_RATE = 5 # Per message
 chatted = []
 
-async def level_up(member):
+async def add_exp(member):
+  global chatted
+  
   if member not in chatted:
-    #logic here
     chatted.append(member)
-  
-  
+    # Need to save the data as well
+    try:
+      data.level_dict[member] += EXP_RATE
+    except KeyError:
+      # They haven't chatted before
+      data.level_dict[member] = EXP_RATE
+
+# Need a non blocking loop here to reset chatted every INTERVAL seconds
+async def clear_chatted_loop():
+    global chatted
+    
+    while True:
+        await asyncio.sleep(INTERVAL)
+        chatted = []
+
