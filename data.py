f85bd119409c6ae90be41d436593bd58770ac024
Bobby-McBobface <54757965+Bobby-McBobface@users.noreply.github.com>
2021-02-24 18:49:58 +0800

    Update data.py

--- data.py
+++ data.py
@@ -1,11 +1,14 @@
 level_dict = {
-  #Format: User ID: XP amount
+  # Format: User ID: XP amount
+  # Also add the current level and amount to next level so we don't have recalculate each time
+  # https://www.desmos.com/calculator/yjvvpuq1jn
 }
 
 warn_dict = {
-  #Format: User ID: [Warning1, Warning2]
+  # Format: User ID: [Warning1, Warning2]
+  # Warning object: <Reason for warn, Timestamp>
 }
 
 mute_dict = {
-  #Format: User ID: Unmute timestamp
+  # Format: User ID: {Timestamp: Unmute timestamp, Previous roles: [Previous role1, Previous role2]
 }
