29bdff3463f12d323896752e17d38d5262cd8bfb
Bobby-McBobface <54757965+Bobby-McBobface@users.noreply.github.com>
2021-02-25 17:59:27 +0800

    Update __init__.py

--- data/__init__.py
+++ data/__init__.py
@@ -1,4 +1,8 @@
 import ast
 
+def save_data(data, filename):
+  with open(filename) as file:
+    file.write(data)
+
 with open('data/levels.txt', 'r') as level_file:
   level_dict = ast.literal_eval(level_file.read())
