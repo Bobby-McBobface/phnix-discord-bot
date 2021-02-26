07c1fa33f2fdfddb489c7396c0634bdccfb0a705
Bobby-McBobface <54757965+Bobby-McBobface@users.noreply.github.com>
2021-02-26 11:14:28 +0800

    Update util.py

--- util.py
+++ util.py
@@ -1,4 +1,5 @@
-async def split_into_member_and_reason(parameters):
+async def split_into_member_and_reason(parameters:str) -> tuple:
+    """Splits parameters into member and reason, used for most moderation commands."""
     split_params = parameters.split(maxsplit=1)
     try:
         member_text = split_params[0]
@@ -15,6 +16,14 @@ async def split_into_member_and_reason(parameters):
         
     return (member, reason)
   
+def check_for_and_strip_prefixes(string:str, prefixes:tuple) -> str:
+    """If `string` starts with one of the given prefixes, return the string sans the prefix. Otherwise, returns None."""
+    for prefix in prefixes:
+        if string.startswith(prefix):
+            return string[len(prefix):].lstrip()
+    # If the loop ended, it failed to find a prefix
+    return None
+
 class warning():
   pass
 
