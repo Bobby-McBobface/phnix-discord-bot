31dcb9840422191649d5190fb8ac239818392500
Bobby-McBobface <54757965+Bobby-McBobface@users.noreply.github.com>
2021-02-28 20:09:14 +0800

    Made kick and ban work with get_member_named

--- util.py
+++ util.py
@@ -1,35 +1,42 @@
 import discord
 
-async def split_into_member_and_reason(parameters:str) -> tuple:
-    """Splits parameters into member and reason, used for most moderation commands."""
+async def split_into_member_and_reason(message, parameters:str) -> tuple:
+    """
+    Splits parameters into member and reason.
+    Used for most moderation commands.
+    """
+    
     if parameters == None:
         return (None, None)
     
-    split_params = parameters.split(maxsplit=1)
-    split_params_pipe = parameters[::1].split("|", 1) # Reversed to split the last one
-    
-    member_id = split_params[0] # There always will be a string here
-    member_text = split_params_pipe[::1].rstrip() # Unreverses it
-    
-    member = discord.guild.get_member(int(member_id.strip('<@!>')))
-    
-    if member = None:
-        member = discord.guild.get_member_named(member_text)
+    try:     
+        split_params = parameters.split(maxsplit=1)
+        member = message.guild.get_member(int(split_params[0].strip('<@!>')))
         try: 
-            reason = split_params_pipe[1]
-            reason = reason[::1] # Unreverse
-        except IndexError:
-            reason = None 
-    else:     
-        try: 
-            reason = split_params[1]
+            reason = split_params[1].lstrip('| ')
         except IndexError:
             reason = None
-   
+        
+    except ValueError:
+        # Reversed to split the last | in case someone has | in their name
+        split_params_pipe = parameters[::-1].split("|", 1)
+        
+        member = message.guild.get_member_named(
+            split_params_pipe[1][::-1].rstrip())
+        
+        try: 
+            reason = split_params_pipe[0][::-1].lstrip()
+        except IndexError:
+            reason = None
+            
     return (member, reason)
   
 async def check_for_and_strip_prefixes(string:str, prefixes:tuple) -> str:
-    """If `string` starts with one of the given prefixes, return the string sans the prefix. Otherwise, returns None."""
+    """
+    If `string` starts with one of the given prefixes,
+    return the string and the prefix. Otherwise, returns None.
+    """
+    
     for prefix in prefixes:
         if string.startswith(prefix):
             return string[len(prefix):].lstrip()
