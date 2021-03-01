49445ef10977977c14ee05858e53f0e841031380
LeeSpork <lblogan@outlook.co.nz>
2021-03-02 10:39:43 +1300

    Added hug messages

--- configuration.py
+++ configuration.py
@@ -8,3 +8,11 @@ COOL_ROLE = 814451206926762014
 # Level/EXP system
 XP_MESSAGE_INTERVAL = 30 # SecondS
 XP_GAIN_PER_MESSAGE = 5
+
+# Messages
+STRINGS_HUG = [
+  # Format: ( "bot's reply", "default target" )
+  ("{hugger} hugs {target}", "themselves"),
+  ("{hugger} gives a big hug to {target}", "themselves, I guess"),
+  ("{hugger} 🫂 {target}", "themself")
+]
