bd87f98e71421caecffb674dc5a68e136f0896f4
Neron Panagiotopoulos <neronios2002@gmail.com>
2021-03-11 15:49:57 +0200

    Checks for user joins and leaves with appropriate messages and mute handling (#2)
    Co-authored-by: Neron <ripstikerpro@gmail.com>
    Co-authored-by: Bobby-McBobface <54757965+Bobby-McBobface@users.noreply.github.com>

--- configuration.py
+++ configuration.py
@@ -5,15 +5,17 @@ MODERATOR_ROLE = 329226272683065346
 EVERYONE_ROLE = 747816685502988339
 COOL_ROLE = 814451206926762014
 MUTED_ROLE = 819176728211226644
+WELCOME_CHANNEL = 819540783656337430
+FAREWELL_CHANNEL = 819540783656337430
 
 # Level/EXP system
-XP_MESSAGE_INTERVAL = 60 # SecondS
+XP_MESSAGE_INTERVAL = 60  # SecondS
 
 # Messages
 STRINGS_HUG = [
-  "{hugger} hugs {target}",
-  "{hugger} gives a big hug to {target}",
-  "{hugger} 🫂 {target}"
+    "{hugger} hugs {target}",
+    "{hugger} gives a big hug to {target}",
+    "{hugger} 🫂 {target}"
 ]
 
 # Mute
@@ -22,6 +24,12 @@ TIME_MULIPLIER = {
     'm': 60,
     'h': 3600,
     'd': 86400,
-    '_': 2635200, # 30.5 days in a month
-    'y': 31557600 # 365.25 days in a year
+    '_': 2635200,  # 30.5 days in a month
+    'y': 31557600  # 365.25 days in a year
 }
+
+########################
+# Preformatted Strings #
+########################
+welcome_msg = "{} has joined the game!"
+farewell_msg = "{} has left the game."
--- main.py
+++ main.py
@@ -1,40 +1,50 @@
 import discord
-import inspect
-import configuration
+
 import commands
+import configuration
 import levels
 import util
-        
+
+
 class PhnixBotClient(discord.Client):
     async def on_ready(self):
         """Runs when the bot is operational"""
         print('PhnixBot is ready')
         await levels.clear_chatted_loop()
-        
+
     async def on_member_join(self, member):
-        pass
-        #todo: welcome message and check mute
-        
+        welcome_channel = self.get_channel(configuration.WELCOME_CHANNEL)
+        await welcome_channel.send(configuration.welcome_msg.format("<@" + str(member.id) + ">"))
+
+        # Check if member is muted and give appropriate role:
+        if util.muted(member):
+            await member.add_roles(member.guild.get_role(configuration.MUTED_ROLE))
+
+    async def on_member_remove(self, member):
+        farewell_channel = self.get_channel(configuration.FAREWELL_CHANNEL)
+        await farewell_channel.send(configuration.farewell_msg.format(member))
+
     async def on_message(self, message):
         """Runs every time the bot notices a message being sent anywhere."""
-        
+
         # Ignore bot accounts
-        if message.author.bot == True: return
-        
-        #EXP/leveling system
+        if message.author.bot:
+            return
+
+        # EXP/leveling system
         await levels.add_exp(message.author.id)
-        
+
         # COMMANDS: Check if it has our command prefix, or starts with a mention of our bot
         command_text = await util.check_for_and_strip_prefixes(
             message.content,
-            (configuration.PREFIX, self.user.mention, f"<@!{self.user.id}>") )
-        
+            (configuration.PREFIX, self.user.mention, f"<@!{self.user.id}>"))
+
         # If there was a command prefix...
         if command_text is not None and command_text != '':
-            
+
             # Split the command into 2 parts, command name and parameters
             split_command_text = command_text.split(maxsplit=1)
-            
+
             command_name = split_command_text[0].lower()
 
             if len(split_command_text) == 2:
@@ -43,39 +53,42 @@ class PhnixBotClient(discord.Client):
             else:
                 # No paramaters specified
                 parameters = None
-            
+
             try:
                 # Get the command's function
                 command_function = commands.command_aliases_dict[command_name]
             except KeyError:
                 # There must not be a command by that name.
                 return
-            
+
             # We got the command's function!
-                
+
             # Do role checks
             for role in message.author.roles:
-                 if role.id in command_function.command_data['role_requirements']:
-                        
-                     # Run the found function
+                if role.id in command_function.command_data['role_requirements']:
+
+                    # Run the found function
                     try:
                         await command_function(message, parameters)
-                        
+
                     except commands.CommandSyntaxError as err:
                         # If the command raised CommandSyntaxError, send some information to the user:
-                        error_details = f": {str(err)}\n" if str(err) != "" else ". " # Get details from the exception, and format it
-                        error_syntax = command_function.command_data['syntax'] # Get command syntax from the function
-                        error_message = f"Invalid syntax{error_details}Usage: `{error_syntax}`" # Put it all together
+                        error_details = f": {str(err)}\n" if str(
+                            err) != "" else ". "  # Get details from the exception, and format it
+                        error_syntax = command_function.command_data['syntax']  # Get command syntax from the function
+                        error_message = f"Invalid syntax{error_details}Usage: `{error_syntax}`"  # Put it all together
                         await message.channel.send(error_message)
-                    
-                    return # So we don't run it more than once
-                
+
+                    return  # So we don't run it more than once
+
             # User does not have permissions to execute that command.
-            roles_string = " or ".join([f"`{message.guild.get_role(role_id).name}`" for role_id in command_function.command_data['role_requirements'] if message.guild.get_role(role_id) != None])
+            roles_string = " or ".join([f"`{message.guild.get_role(role_id).name}`" for role_id in
+                                        command_function.command_data['role_requirements'] if
+                                        message.guild.get_role(role_id) != None])
             await message.channel.send(f"You don't have permission to do that! You need {roles_string}.")
 
-if __name__ == '__main__':
 
+if __name__ == '__main__':
     with open('env/token') as file:
         token = file.read()
 
@@ -90,7 +103,9 @@ if __name__ == '__main__':
         users = True
     )
 
+
     client = PhnixBotClient(intents=intents, allowed_mentions=allowed_mentions)
+
     client.run(token)
 
     print('PhnixBot killed')
