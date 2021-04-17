11bc0dfd08d8427326c36e782620568b7ec3c3a3
Bobby-McBobface <>
2021-04-17 09:38:21 +0800

    level regive on rejoin

--- levels.py
+++ levels.py
@@ -7,7 +7,7 @@ import discord
 chatted = []
 
 
-async def add_exp(member: int, message: discord.Message):
+async def add_exp(member: discord.User, message: discord.Message):
     global chatted
 
     if member.id not in chatted:
@@ -32,37 +32,8 @@ async def add_exp(member: int, message: discord.Message):
 
             await message.channel.send(f"<@!{member.id}> reached level {level-1}! <:poglin:798531675634139176>")
             # Give level roles
-            # Internally, levels are one more than MEE6 was, so there is a compensation
-            if level - 1 == 55:
-                await member.add_roles(member.guild.get_role(configuration.NETHERITE), reason="Level up!")
-                await member.remove_roles(member.guild.get_role(configuration.EMERALD), reason="Level up!")
-            elif level - 1 == 45:
-                await member.add_roles(member.guild.get_role(configuration.EMERALD), reason="Level up!")
-                await member.remove_roles(member.guild.get_role(configuration.OBSIDIAN), reason="Level up!")
-            elif level - 1 == 40:
-                await member.add_roles(member.guild.get_role(configuration.OBSIDIAN), reason="Level up!")
-                await member.remove_roles(member.guild.get_role(configuration.DIAMOND), reason="Level up!")
-            elif level - 1 == 35:
-                await member.add_roles(member.guild.get_role(configuration.DIAMOND), reason="Level up!")
-                await member.remove_roles(member.guild.get_role(configuration.GOLD), reason="Level up!")
-            elif level - 1 == 30:
-                await member.add_roles(member.guild.get_role(configuration.GOLD), reason="Level up!")
-                await member.remove_roles(member.guild.get_role(configuration.LAPIS), reason="Level up!")
-            elif level - 1 == 25:
-                await member.add_roles(member.guild.get_role(configuration.LAPIS), reason="Level up!")
-                await member.remove_roles(member.guild.get_role(configuration.COPPER), reason="Level up!")
-            elif level - 1 == 20:
-                await member.add_roles(member.guild.get_role(configuration.COPPER), reason="Level up!")
-                await member.remove_roles(member.guild.get_role(configuration.IRON), reason="Level up!")
-            elif level - 1 == 15:
-                await member.add_roles(member.guild.get_role(configuration.IRON), reason="Level up!")
-                await member.remove_roles(member.guild.get_role(configuration.STONE), reason="Level up!")
-            elif level - 1 == 10:
-                await member.add_roles(member.guild.get_role(configuration.STONE), reason="Level up!")
-                await member.remove_roles(member.guild.get_role(configuration.WOOD), reason="Level up!")
-            elif level - 1 == 5:
-                await member.add_roles(member.guild.get_role(configuration.WOOD), reason="Level up!")
-                     
+            # Internally, levels are one more than MEE6 was, so there is a compensation  
+            give_level_up_roles(member, level)                 
         
         sqlite_client.execute('''INSERT INTO LEVELS (ID, XP, LEVEL) \
         VALUES(:member, :user_xp, :level) \
@@ -86,3 +57,38 @@ async def clear_chatted_loop():
 
 async def xp_needed_for_level(level: int):
     return int(5/6*((2*(level)**3)+(27*(level)**2)+(91*(level))))
+
+async def give_level_up_roles(member, level):
+    try:
+        if level - 1 >= 55:
+            await member.add_roles(member.guild.get_role(configuration.NETHERITE), reason="Level up!")
+            await member.remove_roles(member.guild.get_role(configuration.EMERALD), reason="Level up!")
+        elif level - 1 >= 45:
+            await member.add_roles(member.guild.get_role(configuration.EMERALD), reason="Level up!")
+            await member.remove_roles(member.guild.get_role(configuration.OBSIDIAN), reason="Level up!")
+        elif level - 1 >= 40:
+            await member.add_roles(member.guild.get_role(configuration.OBSIDIAN), reason="Level up!")
+            await member.remove_roles(member.guild.get_role(configuration.DIAMOND), reason="Level up!")
+        elif level - 1 >= 35:
+            await member.add_roles(member.guild.get_role(configuration.DIAMOND), reason="Level up!")
+            await member.remove_roles(member.guild.get_role(configuration.GOLD), reason="Level up!")
+        elif level - 1 >= 30:
+            await member.add_roles(member.guild.get_role(configuration.GOLD), reason="Level up!")
+            await member.remove_roles(member.guild.get_role(configuration.LAPIS), reason="Level up!")
+        elif level - 1 >= 25:
+            await member.add_roles(member.guild.get_role(configuration.LAPIS), reason="Level up!")
+            await member.remove_roles(member.guild.get_role(configuration.COPPER), reason="Level up!")
+        elif level - 1 >= 20:
+            await member.add_roles(member.guild.get_role(configuration.COPPER), reason="Level up!")
+            await member.remove_roles(member.guild.get_role(configuration.IRON), reason="Level up!")
+        elif level - 1 >= 15:
+            await member.add_roles(member.guild.get_role(configuration.IRON), reason="Level up!")
+            await member.remove_roles(member.guild.get_role(configuration.STONE), reason="Level up!")
+        elif level - 1 >= 10:
+            await member.add_roles(member.guild.get_role(configuration.STONE), reason="Level up!")
+            await member.remove_roles(member.guild.get_role(configuration.WOOD), reason="Level up!")
+        elif level - 1 >= 5:
+            await member.add_roles(member.guild.get_role(configuration.WOOD), reason="Level up!")
+    except:
+        # User probably has the role already
+        pass
