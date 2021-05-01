62ba6f6f5c505d3bccd96eb9e05a4581089fcc30
Bobby-McBobface <>
2021-05-01 12:48:50 +0800

    unmute now tells you if it can't give back roles

--- commands.py
+++ commands.py
@@ -383,7 +383,8 @@ async def unmute(message, parameters, guild=False, silenced=False):
         try:
             await member.add_roles(role)
         except discord.errors.Forbidden:
-            pass
+            if not silenced:
+                await message.channel.send(f"Unable to re-give role: {role.name}")
 
     if guild:
         await member.remove_roles(message.get_role(configuration.MUTED_ROLE))
