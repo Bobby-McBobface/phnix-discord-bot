757608763d06fd3b01d35a31842831c85c57400c
LeeSpork <lblogan@outlook.co.nz>
2021-07-19 15:06:25 +1200

    Update main.py

--- src/main.py
+++ src/main.py
@@ -86,13 +86,15 @@ class PhnixBotClient(discord.Client):
         # Iterate through all words in the blacklist
         for word in configuration.WORDS_CENSORED:
             if word in text_lowercase:
+                # Store the content before we delete it
+                original_content = message.content
                 # Delete messages containing items in WORDS_CENSORED
                 await message.delete()
                 # DM the user what their original message was
                 # with a fancy embed thing
                 censored_word_embed = discord.Embed(
-                    description="Your message was deleted because it contained a blacklisted word.
-                ).add_field(name="Your message", value=message.content)
+                    description="Your message was deleted because it contained a blacklisted word."
+                ).add_field(name="Your message", value=original_content)
                 await message.author.send(embed=censored_word_embed)
                 # Do not continue processing this message
                 return
