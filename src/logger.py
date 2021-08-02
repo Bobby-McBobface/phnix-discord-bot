68214b11d1e3d7739be4b04d198be174221a7ddb
Bobby-McBobface <54757965+Bobby-McBobface@users.noreply.github.com>
2021-08-02 14:16:05 +0800

    made log error an embed to use 4096 chars max

--- src/logger.py
+++ src/logger.py
@@ -5,14 +5,15 @@ import configuration
 
 async def log_moderation(message: discord.Message, warn_embed: discord.Embed, client: discord.Client) -> None:
     moderator = message.author.mention
-    await send_logs(f"Moderator: {moderator}", client, embed=warn_embed)
+    await send_logs(f"Moderator: {moderator}", client, warn_embed)
 
 
 async def log_error(error: str, client: discord.Client) -> None:
-    await send_logs(error, client)
+    error_embed = discord.Embed(title="Error", description=error)
+    await send_logs("", client, error_embed)
 
 
-async def send_logs(message: str, client: discord.Client, embed: Union[discord.Client, None] = None) -> None:
+async def send_logs(message: str, client: discord.Client, embed: Union[discord.Client, None]) -> None:
     try:
         await client.get_guild(configuration.GUILD_ID).get_channel(configuration.LOG_CHANNEL).send(message, embed=embed, allowed_mentions=discord.AllowedMentions(users=False))
     except Exception as e:
