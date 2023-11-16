b43c6af3f7d1fc9dca3f73f01471ac10493efd4f
Bobby-McBobface <54757965+Bobby-McBobface@users.noreply.github.com>
2023-11-16 23:35:06 +0800

    fix: reduce poop cooldown

--- src/cogs/misc.py
+++ src/cogs/misc.py
@@ -19,7 +19,7 @@ class Miscellaneous(commands.Cog):
     """Cog for miscellaneous functionality."""
 
     @commands.hybrid_command()
-    @commands.cooldown(rate=1, per=60 * 60 * 60 * 24, type=BucketType.guild)
+    @commands.cooldown(rate=1, per=7 * 60 * 60 * 24, type=BucketType.guild)
     async def poop(self, ctx: commands.Context):
         """Poop."""
         if random.randint(1, 10) == 10:
